19a8cc03352c66427bb3cdbf007a1c4a4fceb27c
Koichi Yanagisawa <tng62522@gmail.com>
2023-06-30 15:59:23 +0900

    Seleniumを使ったスクレイピングの環境構築完了

--- Docker/src/scraper.rb
+++ Docker/src/scraper.rb
@@ -1,46 +1,25 @@
 # frozen_string_literal: true
 
-# レスポンスをファイルのように扱うgem
-require 'open-uri'
+# selenium-webdriverを使う準備
+require 'selenium-webdriver'
 
-# スクレイピングに使うgem
-require 'nokogiri'
 
-# 出力を見やすくするためのgem
-require 'csv'
+options = Selenium::WebDriver::Chrome::Options.new
 
-# 取得するURLを指定(今回は)
-domain = 'https://qiita.com'
-query = '/search?q=ruby'
-url = domain + query
+options.add_argument('--headless') # ヘッドレスモードを有効にする
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 
-# urlを開いて、resに格納
-res = URI.open(url)
-sleep(1)
+driver = Selenium::WebDriver.for :chrome, options: options
 
-# HTMLをパースする
-body = res.read
-charset = res.charset
-html = Nokogiri::HTML.parse(body, nil, charset)
 
-# 結果を格納する配列
-results = []
-html.css('.style-3f8mxy').each do |node|
-  title = node.css('.style-1toxh2v a').text
-  page = node.css('a.style-1lvpob1').attribute('href').value
-  results << [title, domain + page]
-end
+# # タイムアウトを設定
+# driver.manage.timeouts.implicit_wait = 10
 
-results.each do |result|
-  result_url = result[1]
-  article = URI.open(result_url)
-  sleep(1)
-  article_body = article.read
-  charset = article.charset
-  article_html = Nokogiri::HTML.parse(article_body, nil, charset)
-  puts "タイトル：#{result[0]}"
-  puts "URL：#{result_url}"
-  puts "お気に入り：#{article_html.css('.style-1vpukh3').text}"
-  puts "ストック数：#{article_html.css('.style-1grh9bf').text}"
-  puts '---------------------------------'
-end
+# # リトライ回数を設定
+# driver.manage.timeouts.page_load = 5
+# googleのトップページを開く
+driver.navigate.to 'https://www.google.com/'
+
+# ページのタイトルを取得して表示
+puts driver.title
