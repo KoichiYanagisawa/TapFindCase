97f622d78a1cf35064c00cc98032ecba6af978ee
Koichi Yanagisawa <tng62522@gmail.com>
2023-07-05 14:53:43 +0900

    Railsでバックエンドを実装した

--- Docker/backend/src/config/routes.rb
+++ Docker/backend/src/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  
+  root to: 'products#index'
+  get '/products', to: 'products#index'
+  get '/products/:model', to: 'products#show', as: 'product_model'
+  get '/products/:model/:id', to: 'products#detail', as: 'product_detail'
 end
