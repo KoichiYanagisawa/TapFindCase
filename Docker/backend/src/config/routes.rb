fe974b05b44b936541a3322dd2f96898b428d880
Koichi Yanagisawa <tng62522@gmail.com>
2023-07-16 15:27:51 +0900

    お気に入りのリストを表示できるようにした。

--- Docker/backend/src/config/routes.rb
+++ Docker/backend/src/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/products/detail/:id', to: 'products#detail'
   get '/products/list/:model', to: 'products#modelList'
   get '/products/list/favorite/:user_id', to: 'products#favoriteList'
+  get '/products/list/history/:user_id', to: 'products#historyList'
 
   get '/api/users/:uuid', to: 'users#show'
 
@@ -13,4 +14,6 @@ Rails.application.routes.draw do
   get '/api/favorites/:user_id/:product_id', to: 'favorites#show'
   post '/api/favorites/:user_id/:product_id', to: 'favorites#create'
   delete '/api/favorites/:user_id/:product_id', to: 'favorites#destroy'
+
+  post '/api/history', to: 'history#create'
 end
