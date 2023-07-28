ff6571addc80582baaae1d73a4b55e4adce37bc8
Koichi Yanagisawa <tng62522@gmail.com>
2023-07-28 18:13:58 +0900

    ECRにアップロードするGitHubActionsを作成

--- Docker/url_scraper/src/url_scraper.rb
+++ Docker/url_scraper/src/url_scraper.rb
@@ -11,11 +11,11 @@ class URLScraper
   def initialize
     @urls = Set.new
     @s3 = Aws::S3::Resource.new(
-      region: ENV['AWS_REGION'],
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      region: ENV['MY_AWS_REGION'],
+      access_key_id: ENV['MY_AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['MY_AWS_SECRET_ACCESS_KEY']
     )
-    @bucket = @s3.bucket(ENV['AWS_S3_BUCKET_NAME'])
+    @bucket = @s3.bucket(ENV['BACKEND_AWS_S3_BUCKET'])
   end
 
   def search_item(wait, driver, url)
