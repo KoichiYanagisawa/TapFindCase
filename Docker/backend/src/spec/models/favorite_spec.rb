2075dcb2635fefbfd3a024fa67498a4cc7c1916a
Koichi Yanagisawa <tng62522@gmail.com>
2023-07-30 13:23:17 +0900

    テストの記述方法を修正。

--- Docker/backend/src/spec/models/favorite_spec.rb
+++ Docker/backend/src/spec/models/favorite_spec.rb
@@ -1,43 +1,59 @@
-# require 'rails_helper'
-# require_relative '../../app/models/favorite'
+require 'rails_helper'
+require_relative '../../app/models/favorite'
+require_relative '../../app/services/dynamo_db_client'
+require 'aws-sdk-dynamodb'
 
-# RSpec.describe Favorite, type: :model do
-#   let(:user_id) { 'user1' }
-#   let(:product_id) { 'product1' }
-#   let(:case_name) { 'case1' }
+RSpec.describe Favorite do
+  let(:user_id) { 'user1' }
+  let(:product_id) { 'product1' }
+  let(:dynamodb_client) { instance_double(DynamoDbClient) }
+  let(:put_item_result) { instance_double(Aws::DynamoDB::Types::PutItemOutput) }
+  let(:delete_item_result) { instance_double(Aws::DynamoDB::Types::DeleteItemOutput) }
+  let(:get_item_result) { instance_double(Aws::DynamoDB::Types::GetItemOutput, item: nil) }
+  let(:query_result) { instance_double(Aws::DynamoDB::Types::QueryOutput, items: []) }
 
-#   describe '.create' do
-#     it 'creates a new favorite' do
-#       expect(Favorite.create(user_id: user_id, product_id: product_id)).to eq({ 'product_id' => product_id })
-#     end
-#   end
+  before do
+    allow(DynamoDbClient).to receive(:new).and_return(dynamodb_client)
+    allow(described_class).to receive(:dynamodb).and_return(dynamodb_client)
+    allow(dynamodb_client).to receive(:put_item).and_return(put_item_result)
+    allow(dynamodb_client).to receive(:delete_item).and_return(delete_item_result)
+    allow(dynamodb_client).to receive(:get_item).and_return(get_item_result)
+    allow(dynamodb_client).to receive(:query).and_return(query_result)
+  end
 
-#   describe '.destroy' do
-#     it 'deletes a favorite' do
-#       Favorite.create(user_id: user_id, product_id: product_id)
-#       Favorite.destroy(user_id: user_id, product_id: product_id)
-#       expect(Favorite.find_by(user_id: user_id, product_id: product_id)).to be_nil
-#     end
-#   end
+  describe '.create' do
+    it 'creates a new favorite' do
+      result = described_class.create(user_id:, product_id:)
+      expect(dynamodb_client).to have_received(:put_item)
+      expect(result).to eq({ 'product_id' => product_id })
+    end
+  end
 
-#   describe '.find_by' do
-#     it 'finds a favorite by user_id and product_id' do
-#       Favorite.create(user_id: user_id, product_id: product_id)
-#       expect(Favorite.find_by(user_id: user_id, product_id: product_id)).not_to be_nil
-#     end
-#   end
+  describe '.destroy' do
+    it 'destroys a favorite' do
+      described_class.destroy(user_id:, product_id:)
+      expect(dynamodb_client).to have_received(:delete_item)
+    end
+  end
 
-#   describe '.find_by_user_and_case' do
-#     it 'finds a favorite by user_id and case_name' do
-#       Favorite.create(user_id: user_id, product_id: case_name)
-#       expect(Favorite.find_by_user_and_case(user_id: user_id, case_name: case_name)).to be_truthy
-#     end
-#   end
+  describe '.find_by' do
+    it 'finds a favorite by user_id and product_id' do
+      described_class.find_by(user_id:, product_id:)
+      expect(dynamodb_client).to have_received(:get_item)
+    end
+  end
 
-#   describe '.find_all_by_user' do
-#     it 'finds all favorites by user_id' do
-#       Favorite.create(user_id: user_id, product_id: product_id)
-#       expect(Favorite.find_all_by_user(user_id: user_id)).to include(product_id)
-#     end
-#   end
-# end
+  describe '.find_by_user_and_case' do
+    it 'finds a favorite by user_id and case_name' do
+      described_class.find_by_user_and_case(user_id:, case_name: 'some_case')
+      expect(dynamodb_client).to have_received(:query)
+    end
+  end
+
+  describe '.find_all_by_user' do
+    it 'finds all favorites by user_id' do
+      described_class.find_all_by_user(user_id:)
+      expect(dynamodb_client).to have_received(:query)
+    end
+  end
+end
---
+++ Docker/backend/src/spec/models/product_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+require_relative '../../app/models/product'
+require_relative '../../app/services/dynamo_db_client'
+
+RSpec.describe Product do
+  let(:dynamodb_client) { instance_double(DynamoDbClient) }
+  let(:query_result) { instance_double(Aws::DynamoDB::Types::QueryOutput, items: [], last_evaluated_key: nil) }
+  let(:scan_result) { instance_double(Aws::DynamoDB::Types::ScanOutput, items: []) }
+
+  before do
+    allow(DynamoDbClient).to receive(:new).and_return(dynamodb_client)
+    allow(described_class).to receive(:dynamodb).and_return(dynamodb_client)
+    allow(dynamodb_client).to receive(:query).and_return(query_result)
+    allow(dynamodb_client).to receive(:scan).and_return(scan_result)
+  end
+
+  describe '.query_by_product_name' do
+    it 'queries a product by its name' do
+      described_class.query_by_product_name('product1')
+      expect(dynamodb_client).to have_received(:query)
+    end
+  end
+
+  describe '.all_unique_models' do
+    it 'returns all unique models' do
+      described_class.all_unique_models
+      expect(dynamodb_client).to have_received(:scan)
+    end
+  end
+
+  describe '.find_by' do
+    it 'finds products by field and value' do
+      described_class.find_by('name', 'product1', 'name_index', 'DETAILS')
+      expect(dynamodb_client).to have_received(:query)
+    end
+  end
+end
