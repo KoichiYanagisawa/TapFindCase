2f35b1d69f348787b7f282ebccbfaac7dbbcb6d4
Koichi Yanagisawa <tng62522@gmail.com>
2023-08-09 00:59:08 +0900

    画面リロードすると表示されなくなる不具合を解消した。

--- Docker/frontend/react_app/src/store.js
+++ Docker/frontend/react_app/src/store.js
@@ -1,13 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 const initialState = {
-  userInfo: null
+  userInfo: null,
+  userInfoLoading: false  // 新たに追加したフィールド
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'SET_USER_INFO':
       return { ...state, userInfo: action.payload };
+    case 'SET_USER_INFO_LOADING':  // 新たに追加したアクション
+      return { ...state, userInfoLoading: action.payload };
     default:
       return state;
   }
