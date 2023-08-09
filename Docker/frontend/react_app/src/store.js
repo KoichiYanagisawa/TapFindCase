7500b92ba52c306452ff202327f6ff7359e10ef6
Koichi Yanagisawa <tng62522@gmail.com>
2023-08-09 23:04:58 +0900

    プライバシーポリシーを作成した。

--- Docker/frontend/react_app/src/store.js
+++ Docker/frontend/react_app/src/store.js
@@ -2,14 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 
 const initialState = {
   userInfo: null,
-  userInfoLoading: false  // 新たに追加したフィールド
+  userInfoLoading: false
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'SET_USER_INFO':
       return { ...state, userInfo: action.payload };
-    case 'SET_USER_INFO_LOADING':  // 新たに追加したアクション
+    case 'SET_USER_INFO_LOADING':
       return { ...state, userInfoLoading: action.payload };
     default:
       return state;
